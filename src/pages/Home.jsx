6b21ee353672520d0722ddcb288ccd2a06f2926e
designUon <design_uon@naver.com>
2023-04-28 18:17:47 +0900

    230428

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,10 @@ export default function Home() {
             <MainSlide/>
 
             <ul className='product_warp'>
-                <Link to='/product/speaker'><li><div>SPEAKER</div></li></Link>
-                <Link to='/product/headphone'><li><div>HEADPHONE</div></li></Link>
-                <Link to='/product/earphone'><li><div>EARPHONE</div></li></Link>
-                <Link to='/product/accessory'><li><div>ACCESSORY</div></li></Link>
+                <Link to='/product/'><li><div>SPEAKER</div></li></Link>
+                <Link to='/product/'><li><div>HEADPHONE</div></li></Link>
+                <Link to='/product/'><li><div>EARPHONE</div></li></Link>
+                <Link to='/product/'><li><div>ACCESSORY</div></li></Link>
             </ul>
 
         </div>
