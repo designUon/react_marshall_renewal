58c2ec58bc6b8a112e52080c3cea6f581a61a2ff
designUon <design_uon@naver.com>
2023-05-08 09:53:54 +0900

    230508

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -4,13 +4,13 @@ const ProductContext = React.createContext("");
 
 const ProductContextProvider = ({ children }) => {
     const [productList, setProductList] = useState([
-        { id: 1, name: "Acton-ii", price: 500000, image: "/img/001_marshall-acton-ii-black.png" },
-        { id: 2, name: "Acton-iii", price: 450000, image: "/img/002_marshall-acton-iii-black.png" },
-        { id: 3, name: "Stanmore-iii", price: 400000, image: "/img/003_marshall-stanmore-iii-black.png" },
-        { id: 4, name: "Woburn-ii", price: 350000, image: "/img/004_pl-marshall-speakers-woburn-ii-black-1700x1700.png" },
-        { id: 5, name: "Stanmore-ii", price: 300000, image: "/img/005_pl-marshall-speakers-stanmore-ii-black-1700x1700.png" },
-        { id: 6, name: "Stockwell-ii", price: 250000, image: "/img/006_marshall-stockwell-ii-black-brass.png" },
-        { id: 7, name: "Emberton-ii", price: 200000, image: "/img/007_marshall-emberton-ii-black-brass.png" },
+        { id: 1, name: "Acton-ii", price: 440000, image: "/img/001_marshall-acton-ii-black.png" },
+        { id: 2, name: "Acton-iii", price: 460000, image: "/img/002_marshall-acton-iii-black.png" },
+        { id: 3, name: "Stanmore-iii", price: 600000, image: "/img/003_marshall-stanmore-iii-black.png" },
+        { id: 4, name: "Woburn-ii", price: 880000, image: "/img/004_pl-marshall-speakers-woburn-ii-black-1700x1700.png" },
+        { id: 5, name: "Stanmore-ii", price: 600000, image: "/img/005_pl-marshall-speakers-stanmore-ii-black-1700x1700.png" },
+        { id: 6, name: "Stockwell-ii", price: 399000, image: "/img/006_marshall-stockwell-ii-black-brass.png" },
+        { id: 7, name: "Emberton-ii", price: 289000, image: "/img/007_marshall-emberton-ii-black-brass.png" },
     ].map(product => {
         return {
             ...product,
