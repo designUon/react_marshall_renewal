7e8c82f17c7a31a3cae7bc20f63c7ad74cab9db8
designUon <design_uon@naver.com>
2023-05-10 12:02:03 +0900

    230510 last

--- src/components/NaverLogin.jsx
+++ src/components/NaverLogin.jsx
@@ -22,7 +22,8 @@ const NaverLogin = () => {
         window.open(url, '네이버 간편로그인', options);
 
         // 네이버 로그인 버튼을 눌렀을 때, '/login' 페이지 대신 '/naver-callback' 페이지로 이동하도록 수정합니다.
-        navigate('/naver-callback');
+        // navigate('/naver-callback');
+        navigate('/login');
 
     };
 
